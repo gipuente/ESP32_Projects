bb175d41c0b729057bc42740ab4054e9145c3e74
Ivan Puente <gi.puente777@gmail.com>
2023-04-24 15:24:31 -0300

    pull realizado 24/4/23

--- 03_Freertos/main/main.c
+++ 03_Freertos/main/main.c
@@ -1,13 +1,3 @@
-/*******************************************************************************//**
-    * @file Main.h
-    * @author Ivan Gabriel Puente
-    * @date 26/02/2023
-    * @brief Archivo principal main
-  **********************************************************************************/
-
- /***********************************************************************************
-  * @brief  Librerias asociadas
-  **********************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +5,22 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "ssd1306.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+
+/*
+ You have to set this config value with menuconfig
+ CONFIG_INTERFACE
+ for i2c
+ CONFIG_MODEL
+ CONFIG_SDA_GPIO
+ CONFIG_SCL_GPIO
+ CONFIG_RESET_GPIO
+ for SPI
+ CONFIG_CS_GPIO
+ CONFIG_DC_GPIO
+ CONFIG_RESET_GPIO
+*/
 
  /***********************************************************************************
   * @brief  Defines
@@ -22,14 +28,24 @@
 #define TAG "SSD1306"
 #define IMAGES 10
 #define BLINK_GPIO 2
+#define ADC_1_6 34
+#define ADC_1_7 35
+
+ /***********************************************************************************
+  * @brief  Funciones
+  **********************************************************************************/
 
 
 /***********************************************************************************
   * @brief  Llamados a variables
   **********************************************************************************/
+// !< Estructura de Oled
 SSD1306_t dev;
-TaskHandle_t Task1_Oled = NULL;
-TaskHandle_t Tarea2_Timer = NULL;
+// !< Funcion para caracterizar la calibracion
+esp_adc_cal_characteristics_t adc1_chars;
+// !< Tareas Axis
+TaskHandle_t Task1_Oled_Execution = NULL;
+//TaskHandle_t Tarea2_ShowAxisy = NULL;
 
 /***********************************************************************************
   * @brief  Funciones
@@ -40,12 +56,12 @@ TaskHandle_t Tarea2_Timer = NULL;
   * @param  void *arg 
   * @retval void
   **********************************************************************************/
-void Task1_Oled_Execution(void *arg)
+void Task1_Oled(void *arg)
 {
-    int STATE = 0;
-    int sec = 59;
-    int min = 59;
-    int hour = 1;
+    static int STATE = 0;
+    static int sec = 59;
+    static int min = 59;
+    static int hour = 1;
     char string[10];
     
     while (1)
@@ -74,7 +90,7 @@ void Task1_Oled_Execution(void *arg)
         string[8] = sec%10 + '0';
 
         STATE = ~STATE;
-        ssd1306_display_text(&dev, 0,string, 10, STATE);
+        ssd1306_display_text(&dev, 1,string, 10, STATE);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
@@ -89,112 +105,20 @@ void Task1_Oled_Execution(void *arg)
 
 void app_main(void)
 {
-    SSD1306_t dev;
 
-#if CONFIG_I2C_INTERFACE
-    // !< Mensajes x monitor
-    ESP_LOGI(TAG, "INTERFACE is i2c");
-    ESP_LOGI(TAG, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
-    ESP_LOGI(TAG, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
-    ESP_LOGI(TAG, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-    // !< Inicializacion de comunicacion I2C: 
-    // !< Argumentos: Estructura, GPIO SDA, GPIO SCL, GPIO RESET
-    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
-#endif // CONFIG_I2C_INTERFACE
+    // !< Funciones de inicializacion
+    init_oled();
 
-#if CONFIG_SPI_INTERFACE
-    ESP_LOGI(TAG, "INTERFACE is SPI");
-    ESP_LOGI(TAG, "CONFIG_MOSI_GPIO=%d",CONFIG_MOSI_GPIO);
-    ESP_LOGI(TAG, "CONFIG_SCLK_GPIO=%d",CONFIG_SCLK_GPIO);
-    ESP_LOGI(TAG, "CONFIG_CS_GPIO=%d",CONFIG_CS_GPIO);
-    ESP_LOGI(TAG, "CONFIG_DC_GPIO=%d",CONFIG_DC_GPIO);
-    ESP_LOGI(TAG, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-    // !< Inicializacion de comunicacion I2C: 
-    // !< Argumentos: Estructura, GPIO MOSI, GPIO SCLK, GPIO CS, GPIO DC, GPIO RESET
-    spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO);
-#endif // CONFIG_SPI_INTERFACE
-
-#if CONFIG_FLIP
-    // !< Girar pantalla OLED
-    dev._flip = true;
-    ESP_LOGW(TAG, "Flip upside down");
-#endif
-
-#if CONFIG_SSD1306_128x64
-    ESP_LOGI(TAG, "Panel is 128x64");
-    // !< Inicializacion pantalla ssd1306
-    ssd1306_init(&dev, 128, 64);
-#endif // CONFIG_SSD1306_128x64
-#if CONFIG_SSD1306_128x32
-    ESP_LOGE(TAG, "Panel is 128x32. This demo cannot be run.");
-    while(1) { vTaskDelay(1); }
-#endif // CONFIG_SSD1306_128x32
-
-    // !< Seteamos contraste 0xff -> Contraste maximo
-    ssd1306_contrast(&dev, 0xff);
-
-    // Generamos un buffer liberando espacio en memora: 128 x (8 Paginas de 8 bits) = 8192 bits
-    uint8_t *buffer = (uint8_t *)malloc(8*128); // 8 page 128 pixel
-    if (buffer == NULL) {
-        ESP_LOGE(TAG, "malloc failed");
-        while(1) { vTaskDelay(1); }
-    }
-    // Generamos un buffer de segmento? liberando espacio en memora: 32 x (8 Paginas de 8 bits) = 8192 bits
-    uint8_t *segmentImage = (uint8_t *)malloc(IMAGES*8*32); // 10 image 8 page 32 pixel
-    if (segmentImage == NULL) {
-        ESP_LOGE(TAG, "malloc failed");
-        while(1) { vTaskDelay(1); }
-    }
-
-    // Convert from segmentDisplay to segmentImage
-    for (int imageIndex=0;imageIndex<IMAGES;imageIndex++) {
-        ssd1306_clear_screen(&dev, false);
-        ssd1306_bitmaps(&dev, 0, 8, segmentDisplay[imageIndex], 32, 48, false);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
-
-        // Get from internal buffer to local buffer
-        // buffer is [8][128] 8 page 128 pixel
-        ssd1306_get_buffer(&dev, buffer);
-
-        // Save from buffer to segmentImage
-        // segmentImage is [10][8][32] 10 image 8 page 32 pixel
-        int segmentImageIndex = imageIndex * 256;
-        for (int page=0;page<8;page++) {
-            //ESP_LOGI(TAG, "segmentImageIndex+page*32=%d", segmentImageIndex+page*32);
-            memcpy(&segmentImage[segmentImageIndex+page*32], &buffer[page*128], 32);
-            //ESP_LOGI(TAG, "page=%d", page);
-            //ESP_LOG_BUFFER_HEXDUMP(TAG, &buffer[page*128], 32, ESP_LOG_INFO);
-        }
-
-#if 0
-        ssd1306_clear_screen(&dev, false);
-        for (int page=0;page<8;page++) {
-            ssd1306_display_image(&dev, page, 0, &segmentImage[segmentImageIndex+page*32], 32);
-            //ESP_LOGI(TAG, "page=%d", page);
-            //ESP_LOG_BUFFER_HEXDUMP(TAG, &segmentImage[segmentImageIndex+page*32], 32, ESP_LOG_INFO);
-        }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-#endif
-    }
-
-    // I don't use this anymore
-    free(buffer);
-
-    int digit1 = 0;
-    int digit2 = 0;
-    int digit3 = 0;
-    int digit4 = 0;
-
-    // !< Limpiamos la pantalla
-    ssd1306_clear_screen(&dev, false);
+    // !< Funciones de inicializacion del ADC
+    init_adc();
 
     // !< Creamos la pantalla
-    xTaskCreate(Task1_Oled_Execution, "Task1_Oled_Execution", 4096, NULL, 10, &Task1_Oled);
+    xTaskCreate(Task1_Oled, "Task1_Oled", 4096, NULL, 10, &Task1_Oled_Execution);
 
     // !< Rutina de main  
     while(1) 
     {
-        // !< Dormimos eternamente
-        vTaskDelay(8);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     } 
 }
+
--- 04_Adc/main/main.c
+++ 04_Adc/main/main.c
@@ -1,13 +1,3 @@
-/*******************************************************************************//**
-    * @file Main.h
-    * @author Ivan Gabriel Puente
-    * @date 26/02/2023
-    * @brief Archivo principal main
-  **********************************************************************************/
-
- /***********************************************************************************
-  * @brief  Librerias asociadas
-  **********************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,11 +5,28 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "ssd1306.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+/*
+ You have to set this config value with menuconfig
+ CONFIG_INTERFACE
+ for i2c
+ CONFIG_MODEL
+ CONFIG_SDA_GPIO
+ CONFIG_SCL_GPIO
+ CONFIG_RESET_GPIO
+ for SPI
+ CONFIG_CS_GPIO
+ CONFIG_DC_GPIO
+ CONFIG_RESET_GPIO
+*/
 
  /***********************************************************************************
   * @brief  Defines
   **********************************************************************************/
-#define TAG "SSD1306"
+//#define TAG "SSD1306"
 #define IMAGES 10
 #define BLINK_GPIO 2
 
@@ -29,174 +36,192 @@
   **********************************************************************************/
 // !< Estructura de Oled
 SSD1306_t dev;
+// !< Funcion para caracterizar la calibracion
+esp_adc_cal_characteristics_t adc1_chars;
 // !< Tareas Axis
-TaskHandle_t Tarea1_ShowAxisX = NULL;
-TaskHandle_t Tarea2_ShowAxisy = NULL;
+TaskHandle_t Task1_X = NULL;
+TaskHandle_t Task2_Y = NULL;
+TaskHandle_t Task3_Button = NULL;
+// !< Tag para mandar mensajes x monitor
+static const char *TAG = "ADC EXAMPLE";
 
+enum button_state
+{
+    Button_bounce   = 0,
+    Button_pushed   = 1,
+    Button_relased  = 2,
+
+};
 /***********************************************************************************
   * @brief  Funciones
   **********************************************************************************/
+// !< Tareas Freertos
+void Task1_Adc_X(void *arg);
+void Task2_Adc_Y(void *arg);
+void Task3_PushButton(void *arg);
+
+// !< Funciones de inicializacion
+void init_oled(void);
+void init_adc(void);
+
 /*******************************************************************************//**
-  * @function Task1_Oled_Execution 
+  * @function Task1_ADC_Y 
   * @brief  Tarea que ejecuta un timer. Se ejecuta cada 1 seg. 
   * @param  void *arg 
   * @retval void
   **********************************************************************************/
-void Task1_Oled_Execution(void *arg)
+void Task2_Adc_Y(void *arg)
 {
-    int STATE = 0;
-    int sec = 59;
-    int min = 59;
-    int hour = 1;
-    char string[10];
-    
+    static uint16_t axis_y_mV;
+    char string[12] = " Axis Y: ";
+
     while (1)
     {
-        
-        if (sec<=0){
-            sec = 59;
-            if (min <= 0){
-                min = 59;
-                if (hour <=0){
-                    hour = 59;
-                }
-                else hour--;
-            }
-            else min--;
-        }
-        else sec--;
-        string[0] = ' ';
-        string[1] = hour/10 +'0';
-        string[2] = hour%10 + '0'; 
-        string[3] = ':';
-        string[4] = min/10 + '0';
-        string[5] = min%10 + '0';
-        string[6] = ':';
-        string[7] = sec/10 + '0';
-        string[8] = sec%10 + '0';
+        axis_y_mV = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_6), &adc1_chars);
+        string[9]=  (char)  (axis_y_mV/1000)+'0';
+        string[10]= (char)  ((axis_y_mV-((string[9]-'0')*1000))/100)+'0';
+        string[11]= (char)  ((axis_y_mV-((string[9]-'0')*1000)-((string[10]-'0')*100))/10)+'0';
+        string[12]= (char)  (axis_y_mV-((string[9]-'0')*1000)-((string[10]-'0')*100)-((string[11]-'0')*10))+'0';
+        ESP_LOGI(TAG, "ADC1_CHANNEL_6: %d mV", axis_y_mV);
+        ssd1306_display_text(&dev, 1,string, 13, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}  
+/*******************************************************************************//**
+  * @function Task1_ADC_X 
+  * @brief  Tarea que ejecuta un timer. Se ejecuta cada 1 seg. 
+  * @param  void *arg 
+  * @retval void
+  **********************************************************************************/
+void Task1_Adc_X(void *arg)
+{
+    static uint16_t axis_x_mV;
+    char string[12] = " Axis X: ";
 
-        STATE = ~STATE;
-        ssd1306_display_text(&dev, 0,string, 10, STATE);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    while (1)
+    {
+        axis_x_mV = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_3), &adc1_chars);
+        string[9]=  (char) (axis_x_mV/1000)+'0';
+        string[10]=  (char) ((axis_x_mV-((string[9]-'0')*1000))/100)+'0';
+        string[11]= (char) ((axis_x_mV-((string[9]-'0')*1000)-((string[10]-'0')*100))/10)+'0';
+        string[12]= (char) (axis_x_mV-((string[9]-'0')*1000)-((string[10]-'0')*100)-((string[11]-'0')*10))+'0';
+        ESP_LOGI(TAG, "ADC1_CHANNEL_7: %d mV", axis_x_mV);
+        ssd1306_display_text(&dev, 2,string, 13, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 
+/*******************************************************************************//**
+  * @function Task3_PushButton 
+  * @brief  Tarea que procesa el rebote de un boton
+  * @param  void *arg 
+  * @retval void
+  **********************************************************************************/
+void Task3_PushButton(void *arg)
+{
+    static uint8_t state =  Button_relased;
+    static int8_t bounce_counter = 0;
 
+    while (1)
+    {
+        switch(state)
+        {
+            case Button_relased:
+                if (gpio_get_level(GPIO_NUM_39) == 1) state = Button_bounce;
+                else state = Button_relased;
+                ssd1306_display_text(&dev, 3," Boton = Suelto", 15, 0);
+                break;
+            case Button_bounce:
+                if (gpio_get_level(GPIO_NUM_39) == 1) bounce_counter++;
+                else bounce_counter--;
+                if (bounce_counter >= 15) state = Button_pushed;
+                if (bounce_counter <= -15) state = Button_relased;
+                ssd1306_display_text(&dev, 3," Boton = Rebote", 15, 0);
+                break;
+            case Button_pushed:
+                if (gpio_get_level(GPIO_NUM_39) == 1) state = Button_bounce;
+                else state = Button_pushed;
+                ssd1306_display_text(&dev, 3," Boton = Press ", 15, 0);
+                break;
+            default:
+                break;
+        }
+        ESP_LOGI(TAG, "Button State: %d ", gpio_get_level(GPIO_NUM_39));
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}    
 /*******************************************************************************//**
   * @function app_main 
   * @brief  Main del programa
   * @param  void 
   * @retval void
   **********************************************************************************/
-
 void app_main(void)
 {
-    SSD1306_t dev;
 
-#if CONFIG_I2C_INTERFACE
-    // !< Mensajes x monitor
-    ESP_LOGI(TAG, "INTERFACE is i2c");
-    ESP_LOGI(TAG, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
-    ESP_LOGI(TAG, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
-    ESP_LOGI(TAG, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-    // !< Inicializacion de comunicacion I2C: 
-    // !< Argumentos: Estructura, GPIO SDA, GPIO SCL, GPIO RESET
-    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
-#endif // CONFIG_I2C_INTERFACE
+    // !< Inicializacion de modulos
+    init_oled();
+    init_adc();
 
-#if CONFIG_SPI_INTERFACE
-    ESP_LOGI(TAG, "INTERFACE is SPI");
-    ESP_LOGI(TAG, "CONFIG_MOSI_GPIO=%d",CONFIG_MOSI_GPIO);
-    ESP_LOGI(TAG, "CONFIG_SCLK_GPIO=%d",CONFIG_SCLK_GPIO);
-    ESP_LOGI(TAG, "CONFIG_CS_GPIO=%d",CONFIG_CS_GPIO);
-    ESP_LOGI(TAG, "CONFIG_DC_GPIO=%d",CONFIG_DC_GPIO);
-    ESP_LOGI(TAG, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
-    // !< Inicializacion de comunicacion I2C: 
-    // !< Argumentos: Estructura, GPIO MOSI, GPIO SCLK, GPIO CS, GPIO DC, GPIO RESET
-    spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO);
-#endif // CONFIG_SPI_INTERFACE
-
-#if CONFIG_FLIP
-    // !< Girar pantalla OLED
-    dev._flip = true;
-    ESP_LOGW(TAG, "Flip upside down");
-#endif
-
-#if CONFIG_SSD1306_128x64
-    ESP_LOGI(TAG, "Panel is 128x64");
-    // !< Inicializacion pantalla ssd1306
-    ssd1306_init(&dev, 128, 64);
-#endif // CONFIG_SSD1306_128x64
-#if CONFIG_SSD1306_128x32
-    ESP_LOGE(TAG, "Panel is 128x32. This demo cannot be run.");
-    while(1) { vTaskDelay(1); }
-#endif // CONFIG_SSD1306_128x32
-
-    // !< Seteamos contraste 0xff -> Contraste maximo
-    ssd1306_contrast(&dev, 0xff);
-
-    // Generamos un buffer liberando espacio en memora: 128 x (8 Paginas de 8 bits) = 8192 bits
-    uint8_t *buffer = (uint8_t *)malloc(8*128); // 8 page 128 pixel
-    if (buffer == NULL) {
-        ESP_LOGE(TAG, "malloc failed");
-        while(1) { vTaskDelay(1); }
-    }
-    // Generamos un buffer de segmento? liberando espacio en memora: 32 x (8 Paginas de 8 bits) = 8192 bits
-    uint8_t *segmentImage = (uint8_t *)malloc(IMAGES*8*32); // 10 image 8 page 32 pixel
-    if (segmentImage == NULL) {
-        ESP_LOGE(TAG, "malloc failed");
-        while(1) { vTaskDelay(1); }
-    }
-
-    // Convert from segmentDisplay to segmentImage
-    for (int imageIndex=0;imageIndex<IMAGES;imageIndex++) {
-        ssd1306_clear_screen(&dev, false);
-        ssd1306_bitmaps(&dev, 0, 8, segmentDisplay[imageIndex], 32, 48, false);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
-
-        // Get from internal buffer to local buffer
-        // buffer is [8][128] 8 page 128 pixel
-        ssd1306_get_buffer(&dev, buffer);
-
-        // Save from buffer to segmentImage
-        // segmentImage is [10][8][32] 10 image 8 page 32 pixel
-        int segmentImageIndex = imageIndex * 256;
-        for (int page=0;page<8;page++) {
-            //ESP_LOGI(TAG, "segmentImageIndex+page*32=%d", segmentImageIndex+page*32);
-            memcpy(&segmentImage[segmentImageIndex+page*32], &buffer[page*128], 32);
-            //ESP_LOGI(TAG, "page=%d", page);
-            //ESP_LOG_BUFFER_HEXDUMP(TAG, &buffer[page*128], 32, ESP_LOG_INFO);
-        }
-
-#if 0
-        ssd1306_clear_screen(&dev, false);
-        for (int page=0;page<8;page++) {
-            ssd1306_display_image(&dev, page, 0, &segmentImage[segmentImageIndex+page*32], 32);
-            //ESP_LOGI(TAG, "page=%d", page);
-            //ESP_LOG_BUFFER_HEXDUMP(TAG, &segmentImage[segmentImageIndex+page*32], 32, ESP_LOG_INFO);
-        }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-#endif
-    }
-
-    // I don't use this anymore
-    free(buffer);
-
-    int digit1 = 0;
-    int digit2 = 0;
-    int digit3 = 0;
-    int digit4 = 0;
-
-    // !< Limpiamos la pantalla
-    ssd1306_clear_screen(&dev, false);
+    // !< Seteo el pin 36 como entrada
+    gpio_set_direction(GPIO_NUM_39, GPIO_MODE_INPUT);
 
     // !< Creamos la pantalla
-    xTaskCreate(Task1_Oled_Execution, "Task1_Oled_Execution", 4096, NULL, 10, &Task1_Oled);
+    xTaskCreate(Task1_Adc_X,        "Task1_Adc_X",      4096, NULL, 10, &Task1_X);
+    xTaskCreate(Task2_Adc_Y,        "Task2_Adc_Y",      4096, NULL, 10, &Task2_Y);
+    xTaskCreate(Task3_PushButton,   "Task3_PushButton", 4096, NULL, 10, &Task3_Button);
 
     // !< Rutina de main  
     while(1) 
     {
-        // !< Dormimos eternamente
-        vTaskDelay(8);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     } 
 }
+
+
+
+/*******************************************************************************//**
+  * @function init_oled 
+  * @brief  Inicializacion de una pantalla oled
+  * @param  void 
+  * @retval void
+  **********************************************************************************/
+void init_oled(void)
+{
+    // !< Inicializacion de comunicacion I2C: 
+    // !< Argumentos: Estructura, GPIO SDA, GPIO SCL, GPIO RESET
+    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
+
+    // !< Girar pantalla OLED
+    //dev._flip = true;
+
+    // !< Inicializacion pantalla ssd1306
+    ssd1306_init(&dev, 128, 64);
+
+    // !< Seteamos contraste 0xff -> Contraste maximo
+    ssd1306_contrast(&dev, 0xff);
+
+    // !< Limpiamos la pantalla
+    ssd1306_clear_screen(&dev, false);
+
+} 
+
+/*******************************************************************************//**
+  * @function init_adc 
+  * @brief  Inicializacion del adc
+  * @param  void 
+  * @retval void
+  **********************************************************************************/
+void init_adc(void)
+{
+    // !< Calibro el ADC 1
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+    
+    // !< Configuro la conversion de adc a 12 bits
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+
+    // !< Configuro la atenuacion a 11db, mido el rango maximo
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11));
+
+} 
